68e90c7a05be91b6fd235e0ad4261da6debcc280
ByZeWay <77978995+ByZeWay@users.noreply.github.com>
2024-10-28 16:10:00 +0300

    lab-2

---
+++ lab-2/task_1.py
@@ -0,0 +1,19 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+
+months = 0
+isPositiveBudget = True
+
+while isPositiveBudget:
+    budget = money_capital + salary
+
+    if budget >= spend:
+        months += 1
+        money_capital -= spend - salary
+        spend = spend + spend * increase
+    else:
+        break
+
+print("Количество месяцев, которое можно протянуть без долгов:", months)
