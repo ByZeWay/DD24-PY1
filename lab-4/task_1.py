73a36dc2fd13e3dfc22d6ecb26fa2ea592b69839
ByZeWay <77978995+ByZeWay@users.noreply.github.com>
2024-11-17 19:02:59 +0300

    Update task_1.py

--- lab-4/task_1.py
+++ lab-4/task_1.py
@@ -4,12 +4,7 @@ def task() -> float:
     with open('input.json', 'r') as file:
         data = json.load(file)
 
-    s = 0.0
-
-    for i in data:
-        score = i.get("score", 0)
-        weight = i.get("weight", 0)
-        s += score * weight
+    s = sum([item["score"] * item["weight"] for item in data])
 
     return round(s, 3)
 
